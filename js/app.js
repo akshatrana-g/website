ab9198c49fd24e81f68fce293d626361711d298e
Akshat-Rana <akshat511716@gmail.com>
2021-06-15 17:05:32 +0530

    Added toggle light and dark mode icon

--- js/app.js
+++ js/app.js
@@ -1,64 +1,53 @@
-console.log('working')
+console.log("working");
 
-
-let theme = localStorage.getItem('theme');
-if(theme == null){
-    setTheme('light')
-}
-else{
-    setTheme(theme)
+let theme = localStorage.getItem("theme");
+if (theme == null) {
+  setTheme("light");
+} else {
+  setTheme(theme);
 }
 
-// var themeDots = document.getElementsByClassName('theme');
-var change = document.querySelector("#nav-content nav ul li svg")
+var changeToDark = document.querySelector("#nav-content nav ul .light");
+var changeToLight = document.querySelector("#nav-content nav ul .dark");
 
-change.addEventListener('click', function(){
-    if(change.classList.contains("light")){
-        change.classList.remove("light");
-        change.classList.add("dark");
-        setTheme("dark");
-    }
-    else if(change.classList.contains("dark")){
-        change.classList.remove("dark");
-        change.classList.add("light");
-        setTheme("light");
-    }
-})
+changeToDark.addEventListener("click", function () {
+  changeToDark.classList.add("hide");
+  changeToLight.classList.remove("hide");
+  setTheme("dark");
+});
+changeToLight.addEventListener("click", function () {
+  changeToLight.classList.add("hide");
+  changeToDark.classList.remove("hide");
+  setTheme("light");
+});
 
-function setTheme(mode){
-    if (mode == 'light'){
-        document.getElementById('theme-style').href = 'css/style.css'
-    }
-    if (mode == 'dark'){
-        document.getElementById('theme-style').href = 'css/dark.css'
-    }
-    localStorage.setItem('theme', mode)
+function setTheme(mode) {
+  if (mode == "light") {
+    document.getElementById("theme-style").href = "css/style.css";
+  }
+  if (mode == "dark") {
+    document.getElementById("theme-style").href = "css/dark.css";
+  }
+  localStorage.setItem("theme", mode);
 }
 
+const hamburgerMenu = document.querySelector("#navigation .nav-icon");
+const navContent = document.querySelector("#nav-content");
+const closeNavButton = document.querySelector("#nav-content .close-btn");
+const navLinks = document.querySelectorAll("#nav-content nav ul li a");
 
+hamburgerMenu.addEventListener("click", () => {
+  document.body.style.overflow = "hidden";
+  navContent.classList.add("show");
+});
+closeNavButton.addEventListener("click", () => {
+  document.body.style.overflow = "auto";
+  navContent.classList.remove("show");
+});
 
-const hamburgerMenu = document.querySelector('#navigation .nav-icon');
-const navContent = document.querySelector('#nav-content');
-const closeNavButton = document.querySelector('#nav-content .close-btn');
-const navLinks = document.querySelectorAll('#nav-content nav ul li a');
-
-
-
-
-hamburgerMenu.addEventListener('click', ()=>{
-    document.body.style.overflow = 'hidden';
-    navContent.classList.add('show');
-})
-closeNavButton.addEventListener('click', ()=>{
-    document.body.style.overflow = 'auto'
-    navContent.classList.remove('show');
-})
-
-
-navLinks.forEach( link => {
-    link.addEventListener('click', ()=>{
-        document.body.style.overflow = "auto";
-        navContent.classList.remove('show');
-    })
-})
-
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.style.overflow = "auto";
+    navContent.classList.remove("show");
+  });
+});
